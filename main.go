2027e6ee8242d2fdbdafd671e76bad4ccbc8e86d
Paul <paulruales1@gmail.com>
2022-04-21 22:13:59 -0400

    deploy to fly

--- main.go
+++ main.go
@@ -2,17 +2,33 @@ package main
 
 import (
 	"log"
+	"os"
 
 	"github.com/gofiber/fiber/v2"
+	"github.com/joho/godotenv"
 )
 
 func main () {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+	}
+
 	app := fiber.New()
+
+	app.Use(func(c *fiber.Ctx) error {
+		return c.SendStatus(404)
+	})
 
 	app.Get("/", func(c *fiber.Ctx) error {
 		return c.SendString("Hello, World!")
 	})
 
-	log.Println("Listening on port 3000")
-	log.Fatal(app.Listen(":3000"))
+	log.Println("Listening on port", port)
+	log.Fatal(app.Listen(":" + port))
 }
